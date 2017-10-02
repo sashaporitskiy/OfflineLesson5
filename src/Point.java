bb07073addaa2cd44167c6c889d45e3b47bfbdda
sashaporitskiy <31014377+sashaporitskiy@users.noreply.github.com>
2017-10-02 20:12:12 +0300

    relys1

--- src/Point.java
+++ src/Point.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Point {
+    float x;
+    float y;
+
+    public Point(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+    float calcLength(Point otherPoint){
+
+        return (float) Math.sqrt(Math.pow((this.y-otherPoint.y),2)+Math.pow((this.x-otherPoint.x),2));
+
+    }
+
 }
