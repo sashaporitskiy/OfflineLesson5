306ce35072d7c1975a2997dd38a2aeb1b90088b4
sashaporitskiy <31014377+sashaporitskiy@users.noreply.github.com>
2017-10-02 20:12:53 +0300

    1

---
+++ src/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+        Point p1 = new Point(2,2);
+        Point p2 = new Point(1,1);
+
+        System.out.println(p1.calcLength(p2));
+        System.out.println("------------------------");
+
+    }
+}
